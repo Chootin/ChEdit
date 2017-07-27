9b1b453df5f43c238141fd20c47629bcde0f8aad
Chootin <alantullin@gmail.com>
2017-07-27 13:02:35 +0000

    More input testing.

--- inputtest.c
+++ inputtest.c
@@ -1,49 +1,27 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <termios.h>
 #include <string.h>
 #include <ncurses.h>
 
-int getkey() {
-        int character;
-        struct termios orig_term_attr;
-        struct termios new_term_attr;
-
-        /* set the terminal to raw mode */
-        tcgetattr(fileno(stdin), &orig_term_attr);
-        memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
-        new_term_attr.c_lflag &= ~(ECHO|ICANON);
-        new_term_attr.c_cc[VTIME] = 0;
-        new_term_attr.c_cc[VMIN] = 0;
-        tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
-
-        /* read a character from the stdin stream without blocking */
-        /*   returns EOF (-1) if no character is available */
-        character = fgetc(stdin);
-
-        /* restore the original terminal attributes */
-        tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
-
-        return character;
-}
-
 int main(int argc, char *argv[]) {
-	char flip = 0;
-	int ch;
+	initscr();
 	noecho();
+	int *ch = (int *) malloc(10 * sizeof(int));
+	int index = 0;
 	while (1) {
-		if (flip) {
-			ch = getkey();
-		} else {
-			ch = getchar();
-		}
-
-		if (ch != -1) {
-			flip = !flip;
-		}
+		index = -1;
+		do {
+			ch[++index] = getch();
+		} while (ch[index] != -1 && ch[index] != 0);
 		
-		printf("%d        %c\n", ch ,ch);
+		
+		if (ch[0] != -1 && ch[1] != 0) {
+			clear();
+			mvprintw(0, 0, "%d %d %d %d %d %d %d %d %d %d\n", ch[0], ch[1], ch[2], ch[3], ch[4], ch[5], 
+			ch[6], ch[7], ch[8], ch[9]);
+			refresh();
+		}
 	}
 	return 0;	
 }	
