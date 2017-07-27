7caf0950b9b0313f005552708ae3401bdf3804fd
Alec Tutin <alec.tutin@gbst.com>
2017-07-27 14:15:43 +1000

    Uploading code fragment for testing

--- input.c
+++ input.c
@@ -1,7 +1,33 @@
 #include "input.h"
 #include <curses.h>
+#include <termios.h>
+#include <string.h>
 #include "document.h"
 #include "cursor.h"
+
+
+int getkey() {
+	int character;
+	struct termios orig_term_attr;
+	struct termios new_term_attr;
+
+	/* set the terminal to raw mode */
+	tcgetattr(fileno(stdin), &orig_term_attr);
+	memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
+	new_term_attr.c_lflag &= ~(ECHO|ICANON);
+	new_term_attr.c_cc[VTIME] = 0;
+	new_term_attr.c_cc[VMIN] = 0;
+	tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
+
+	/* read a character from the stdin stream without blocking */
+	/*   returns EOF (-1) if no character is available */
+	character = fgetc(stdin);
+
+	/* restore the original terminal attributes */
+	tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
+
+	return character;
+}
 
 /**
 *** Command key combinations for ChEdit
